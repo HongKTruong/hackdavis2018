256e2faf780c43e3c28f672f806303fb5c7cb060
richardgao12345 <richard.gao12345@gmail.com>
2018-01-20 21:18:13 -0800

    Camera fully loads on screen and capture button on bottom

--- FoodSnapperz/App.js
+++ FoodSnapperz/App.js
@@ -47,14 +47,7 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    flexDirection: 'row',
   },
   instructions: {
     textAlign: 'center',
